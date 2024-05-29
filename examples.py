ead0fb7f05a8bf3b2d224f042663449084b8e30e
Aaron <aaron.osrin@post.runi.ac.il>
2024-05-29 16:51:21 +0300

    third commit - clean code

--- examples.py
+++ examples.py
@@ -1,54 +1,15 @@
 import numpy as np
 
 class Examples:
-    def ackley(self, x):
-        a = 20
-        b = 0.2
-        c = 2 * np.pi
-        d = len(x)
-        sum1 = np.sum(x**2)
-        sum2 = np.sum(np.cos(c * x))
-        term1 = -a * np.exp(-b * np.sqrt(sum1 / d))
-        term2 = -np.exp(sum2 / d)
-        return term1 + term2 + a + np.exp(1)
-    
-    def ackley_grad(self, x):
-        a = 20
-        b = 0.2
-        c = 2 * np.pi
-        d = len(x)
-        sum1 = np.sum(x**2)
-        grad1 = a * b * np.exp(-b * np.sqrt(sum1 / d)) * (x / (d * np.sqrt(sum1 / d)))
-        grad2 = np.exp(np.sum(np.cos(c * x)) / d) * (c / d) * np.sin(c * x)
-        return grad1 + grad2
-
-    def quadratic(self, x):
-        Q = np.eye(len(x))
-        return 0.5 * np.dot(x, Q.dot(x))
 
     def abs_function(self, x):
         return (np.abs(x[0])+np.abs(x[1]))
-    
-    def quadratic_grad(self, x):
-        Q = np.eye(len(x))
-        return Q.dot(x)
-    
-    def rosenbrock(self, x):
-        return sum(100.0 * (x[1:] - x[:-1]**2.0)**2.0 + (1 - x[:-1])**2.0)
-
-    def rosenbrock_grad(self, x):
-        grad = np.zeros_like(x)
-        grad[0] = -400 * x[0] * (x[1] - x[0]**2) - 2 * (1 - x[0])
-        grad[1] = 200 * (x[1] - x[0]**2)
-        return grad
     
     def sphere(self, x):
         return sum(x**2)
     
     def sphere_grad(self, x):
         return 2 * x
-
-
 
     def beale(self, x):
         return (1.5 - x[0] + x[0] * x[1])**2 + (2.25 - x[0] + x[0] * x[1]**2)**2 + (2.625 - x[0] + x[0] * x[1]**3)**2
@@ -64,7 +25,7 @@ class Examples:
 
         df_dx = 2 * f1 * df1_dx + 2 * f2 * df2_dx + 2 * f3 * df3_dx
 
-        # Partial derivative with respect to y
+        
         df1_dy = x[0]
         df2_dy = 2 * x[0] * x[1]
         df3_dy = 3 * x[0] * x[1]**2
@@ -72,29 +33,3 @@ class Examples:
         df_dy = 2 * f1 * df1_dy + 2 * f2 * df2_dy + 2 * f3 * df3_dy
 
         return np.array([df_dx, df_dy])
-        
-
-    def booth(self, x):
-        return (x[0] + 2*x[1] - 7)**2 + (2*x[0] + x[1] - 5)**2
-
-    def matyas(self, x):
-        return 0.26 * (x[0]**2 + x[1]**2) - 0.48 * x[0] * x[1]
-
-    def himmelblau(self, x):
-        return (x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2
-
-    def levi(self, x):
-        return np.sin(3 * np.pi * x[0])**2 + (x[0] - 1)**2 * (1 + np.sin(3 * np.pi * x[1])**2) + (x[1] - 1)**2 * (1 + np.sin(2 * np.pi * x[1])**2)
-
-    def three_hump_camel(self, x):
-        return 2 * x[0]**2 - 1.05 * x[0]**4 + (x[0]**6) / 6 + x[0] * x[1] + x[1]**2
-
-    def rastrigin(self, x):
-        A = 10
-        return A * len(x) + sum(x**2 - A * np.cos(2 * np.pi * x))
-
-    def griewank(self, x):
-        return 1 + sum(x**2) / 4000 - np.prod(np.cos(x / np.sqrt(np.arange(1, len(x) + 1))))
-
-    def schaffer_n2(self, x):
-        return 0.5 + (np.sin(x[0]**2 - x[1]**2)**2 - 0.5) / (1 + 0.001 * (x[0]**2 + x[1]**2))**2
--- test_pattern_search.py
+++ test_pattern_search.py
@@ -9,7 +9,6 @@ class TestPatternSearch(unittest.TestCase):
 
     def setUp(self):
         self.examples = Examples()
-        self.x0 = np.array([1.5, 1.5])
         self.gamma0 = 1.0
         self.gamma_tol = 1e-6
         self.theta_max = 0.5
@@ -19,64 +18,35 @@ class TestPatternSearch(unittest.TestCase):
         self.alpha = 0.01
         self.tol = 1e-6
 
-    def run_pattern_search(self, func, grad, name):
-        psm = PatternSearch(func, self.x0, self.gamma0, self.gamma_tol, self.theta_max, self.rho, self.phi, self.max_iter)
+    def runner(self, func, grad, name,x0, x_plot, y_plot):
+        psm = PatternSearch(func, x0, self.gamma0, self.gamma_tol, self.theta_max, self.rho, self.phi, self.max_iter)
         x_min_psm, f_min_psm, path_psm, function_values_psm = psm.search()
         
         if grad is not None:
-            gd = GradientDescent(func, grad, self.x0, self.alpha, self.tol, self.max_iter)
+            gd = GradientDescent(func, grad, x0, self.alpha, self.tol, self.max_iter)
             x_min_gd, f_min_gd, path_gd, function_values_gd = gd.search()
             print(f"{name} - Pattern Search Minimum: {x_min_psm}, Function value: {f_min_psm}")
             print(f"{name} - Gradient Descent Minimum: {x_min_gd}, Function value: {f_min_gd}")
-            plot_contour_with_paths(func, [-4, 4, -2, 2], name, paths=[(path_psm, "Pattern Search"), (path_gd, "Gradient Descent")])
+            plot_contour_with_paths(func, [-x_plot, x_plot, -y_plot, y_plot], name, paths=[(path_psm, "Pattern Search"), (path_gd, "Gradient Descent")])
             plot_function_values(name, (function_values_psm, "Pattern Search"), (function_values_gd, "Gradient Descent"))
         else:
             print(f"{name} - Pattern Search Minimum: {x_min_psm}, Function value: {f_min_psm}")
-            plot_contour_with_paths(func, [-2, 2, -2, 2], name, paths=[(path_psm, "Pattern Search")])
+            plot_contour_with_paths(func, [-x_plot, x_plot, -y_plot, y_plot], name, paths=[(path_psm, "Pattern Search")])
             plot_function_values(name, (function_values_psm, "Pattern Search"))
 
-    #def test_ackley(self):
-    #    self.run_pattern_search(self.examples.ackley, self.examples.ackley_grad, "Ackley")
-
-    #def test_quadratic(self):
-    #    self.run_pattern_search(self.examples.quadratic, self.examples.quadratic_grad, "Quadratic")
-
-   #def test_rosenbrock(self):
-    #    self.run_pattern_search(self.examples.rosenbrock, self.examples.rosenbrock_grad, "Rosenbrock")
 
     def test_sphere(self):
-        self.run_pattern_search(self.examples.sphere, self.examples.sphere_grad, "Sphere")
+        x0 = np.array([1.5,1.5])
+        self.runner(self.examples.sphere, self.examples.sphere_grad, "Sphere",x0,  2, 2)
 
-    # For functions without gradients, we'll pass None
+    
     def test_beale(self):
-        self.run_pattern_search(self.examples.beale, self.examples.beale_grad, "Beale")
-
-    #def test_booth(self):
-    #    self.run_pattern_search(self.examples.booth, None, "Booth")
-
-    #def test_matyas(self):
-    #    self.run_pattern_search(self.examples.matyas, None, "Matyas")
-
-    #def test_himmelblau(self):
-    #    self.run_pattern_search(self.examples.himmelblau, None, "Himmelblau")
-
-    #def test_levi(self):
-    #    self.run_pattern_search(self.examples.levi, None, "Levi")
-
-    #def test_three_hump_camel(self):
-    #    self.run_pattern_search(self.examples.three_hump_camel, None, "Three-Hump Camel")
-
-    #def test_rastrigin(self):
-    #    self.run_pattern_search(self.examples.rastrigin, None, "Rastrigin")
-
-    #def test_griewank(self):
-     #   self.run_pattern_search(self.examples.griewank, None, "Griewank")
-
-    #def test_schaffer_n2(self):
-    #    self.run_pattern_search(self.examples.schaffer_n2, None, "Schaffer N. 2")
+        x0 = np.array([0,0])
+        self.runner(self.examples.beale, self.examples.beale_grad, "Beale",x0,  4.5, 2)
     
     def test_abs_function(self):
-        self.run_pattern_search(self.examples.abs_function, None, "Absolute Value")
+        x0 = np.array([1.5,1.5])
+        self.runner(self.examples.abs_function, None, "Absolute Value",x0,  2, 2)
 
 if __name__ == '__main__':
     unittest.main()
